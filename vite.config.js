cef26850836648f2ace1586b9193df3a2cdfe23f
CarlosCG <c.g.cabrera.gallardo@gmail.com>
2026-09-20 17:11:19 -0500

    Corrección vite.config.js para lograr deploy

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(), // 2. Add it here
   ],
-  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
